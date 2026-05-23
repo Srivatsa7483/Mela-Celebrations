6b85ebb7d52d917752a0cbb1ccee6db1bc60056e
Srivatsa7483 <srivatsa7483@example.com>
2026-05-23 15:09:31 +0530

    fix(mail): automatically use nodemailer service: 'gmail' for optimized connection

--- server/index.js
+++ server/index.js
@@ -46,6 +46,11 @@ const smtpConfig = {
     : undefined,
 };
 
+// If using Gmail, let Nodemailer use its optimized built-in Gmail service configuration!
+if (smtpConfig.host && smtpConfig.host.toLowerCase().includes("gmail")) {
+  smtpConfig.service = "gmail";
+}
+
 
 const emailTransporter = smtpConfig.host && smtpConfig.auth
   ? nodemailer.createTransport(smtpConfig)
